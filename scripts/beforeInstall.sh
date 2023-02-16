dc73de382cfc94ef07049c9a7571f6e46fcd653a
Maria <91953276+mariasnchez@users.noreply.github.com>
2023-02-16 10:41:36 +0100

    Add files via upload

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,8 +1,13 @@
-#!/bin/bash -xe
+#!/bin/bash
+
+# Start Install
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. ~/.nvm/nvm.sh
-. ~/.bashrc
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+#Instalando al ultima version de node
 nvm install --lts
-ln -sf "SNVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
-ln -sf "SNVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
-mkdir -p /home/ubuntu/app/release/logs
+
+ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
+ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
